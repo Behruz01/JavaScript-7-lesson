9fd5d7d10cc3f8635a3abfc647830c90522c10a1
Behruz01 <ibragimovbehruz822@gmail.com>
2023-01-05 23:42:16 +0500

    changed

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-let inp1 = document.querySelector("#inp1");
-let inp2 = document.querySelector("#inp2");
-let inp3 = document.querySelector("#inp3");
-let input = document.querySelector("contact__inp");
-let btn = document.querySelector(".contact__btn");
-let border = document.querySelector(".contact__inform");
-let forName = document.querySelector(".contact__info");
-let forRelat = document.querySelector(".contact__text");
-let forPhone = document.querySelector(".contact__number");
-function import1() {
-  if (input !== "") {
-    forName.textContent = inp1.value;
-    forRelat.textContent = inp2.value;
-    forPhone.textContent = inp3.value;
-  } else {
-    forName.textContent = "Qiymat kiriting";
+const elForm = document.querySelector(".contact__form");
+const elName = document.querySelector("#inp1");
+const elSurname = document.querySelector("#inp2");
+const elPhone = document.querySelector("#inp3");
+const button = document.querySelector(".contact__btn");
+const elContact = document.querySelector(".contact__right");
+
+const contacts = [];
+elForm.addEventListener("submit", function (evn) {
+  evn.preventDefault();
+  if (elName.value !== "" && elSurname.value !== "" && elPhone.value !== "") {
+    elContact.innerHTML = `<h2>Contacts</h2>`;
+    let contactName = elName.value;
+    let contactSurname = elSurname.value;
+    let contactPhone = elPhone.value;
+
+    contacts.push({ contactName, contactSurname, contactPhone });
+    for (let i = 0; i < contacts.length; i++) {
+      elContact.innerHTML += `
+              <div style="margin-bottom: 30px" class="contact__inform">
+                <h3 class="contact__info">${contacts[i].contactName}</h3>
+                <p class="contact__text">${contacts[i].contactSurname}</p>
+                <a href="#" class="contact__number">${contacts[i].contactPhone}</a>
+              </div>`;
+      elName.value = "";
+      elSurname.value = "";
+      elPhone.value = "";
+    }
   }
-}
-btn.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  import1();
 });
